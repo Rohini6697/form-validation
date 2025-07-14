52e4bca38f0f2a7a88d4ee8f7cee272cd4b3a068
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-07-14 16:27:33 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const valid=()=>{
+    const user = document.getElementById('user').value;
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+    const cpass = document.getElementById('cpass').value;
+
+    if(!user,!email,!pass,!cpass){
+        alert('data incomplete')
+    }
+
+    if(pass!==cpass){
+        alert('Password not matching');
+    }
+    if(pass.lenght>6){
+        alert('password must be 6 character long')
+    }
+    if(!email.includes('@')){
+        alert('Email is not valid')
+    }
+    if(!user.match(/^[a-zA-Z0-9]+$/)){
+        alert('Username must be alpha numeric')
+    }
+
+    
+}
