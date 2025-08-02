7235c60c3ac1281eef5b13385e28a83853b42558
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-08-02 22:57:28 +0530

    changes

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const valid=()=>{
     const email = document.getElementById('email').value;
     const pass = document.getElementById('pass').value;
     const cpass = document.getElementById('cpass').value;
-
-    // if(!user || !email || !pass || !cpass){
-    //     document.getElementById("use").innerHTML = 'Data incomplete. All fields are mandatory';
-    //     document.getElementById("demo").innerHTML = 'Data incomplete. All fields are mandatory';
-    //     document.getElementById("cp").innerHTML = 'Data incomplete. All fields are mandatory';
-    //     document.getElementById("pas").innerHTML = 'Data incomplete. All fields are mandatory';
-
-    //     document.getElementById("user").style.border ="3px solid red";
-    //     document.getElementById("email").style.border ="3px solid red";
-    //     document.getElementById("cpass").style.border ="3px solid red";
-    //     document.getElementById("pass").style.border ="3px solid red";
-        
-    //     return;
-    // }
     if (!user) {
         document.getElementById("use").innerHTML = 'Please fill the Username field!';
         return;
